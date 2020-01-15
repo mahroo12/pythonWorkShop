70f22586f56db3b814da9043a93e09f27122fc73
mahroo12 <mahroo.rahman@gmail.com>
2020-01-14 19:12:52 -0500

    first python code

---
+++ calculator.py
@@ -0,0 +1,29 @@
+import numpy as np
+A = np.array([[1,1], [0,1]])
+mystring = 'McGill'
+print(mystring)
+print(mystring.lower())
+print(type(mystring))
+a = ''
+b = 5
+c = 6
+print(c + b)
+
+mynumber = 1
+mynumber2 = 1.
+
+mylist = [1, 1., 'str']
+print(mylist)
+print(type(mylist))
+
+mydict = {}
+print(type(mydict))
+
+mydict['myfirstkey'] = 1
+print((mydict))
+
+print(mydict[mydict])
+
+
+
+
